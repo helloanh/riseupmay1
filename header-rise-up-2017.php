747273ee26e98ce0ed1eb8ef1ef0b9391889637f
Anh K <akhoang88@gmail.com>
2017-04-04 15:36:26 -0400

    Create header-rise-up-2017.php

---
+++ header-rise-up-2017.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Rise Up May 1st Day of Action. We resist. We RISE UP. Join us May 1 to stand with immigrants everywhere!">
+    <meta name="author" content="FIRM, RIFA, CCCA">
+    <!-- facebook meta -->
+    <meta property="og:description" content="Our country is strongest when we are united. Together we share a common future. Yet certain politicians seek to set us against one another for their personal gain and profit. We resist. We RISE UP. Join us May 1 to stand with immigrants everywhere!">
+    <meta property="og:title" content="Rise Up May 1st" />
+    <meta property="og:url" content="https://reformimmigrationforamerica.org/rise-up/" />
+    <meta property="og:image" content="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/meta-banner-img.jpg" />
+    <!-- twitter meta -->
+    <meta name="twitter:card" content="Rise Up May !st Day of Action">
+    <meta name="twitter:url" content="https://reformimmigrationforamerica.org/rise-up/">
+    <meta name="twitter:title" content="Rise Up May 1st">
+    <meta name="twitter:description" content="Our country is strongest when we are united. Together we share a common future. Yet certain politicians seek to set us against one another for their personal gain and profit. We resist. We RISE UP. Join us May 1 to stand with immigrants everywhere!">
+    <meta name="twitter:image" content="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/meta-banner-img.jpg">
+    <title>Rise Up May 1 </title>
+    <!-- Bootstrap Core CSS -->
+    <link href="https://reformimmigrationforamerica.org/wp-content/themes/ri4a2015/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="https://reformimmigrationforamerica.org/wp-content/themes/ri4a2015/lib/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://reformimmigrationforamerica.org/wp-content/themes/ri4a2015/lib/simple-line-icons/css/simple-line-icons.css">
+    <!-- Theme CSS -->
+    <link href="https://reformimmigrationforamerica.org/wp-content/themes/ri4a2015/stylesheets/css/rise-up.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- Blue State Digital FORM SET UP -->
+    <link rel="canonical" href="http://we.reformimmigrationforamerica.org/page/s/-rise-up-day-of-action">
+    <link rel="stylesheet" href="https://s.bsd.net/page/static/20170314.3/skins/bsd-light/signup/frontend.css" type="text/css">
+    <link rel="stylesheet" href="https://s.bsd.net/page/static/20170314.3/ext/yui/build/container/assets/skins/sam/container.css" type="text/css">
+    <link rel="stylesheet" href="https://s.bsd.net/page/static/20170314.3/utils/blue_captcha/css/blue_captcha.css" type="text/css">
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/yui/build/yahoo-dom-event/yahoo-dom-event.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/yui/build/dragdrop/dragdrop-min.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/yui/build/container/container-min.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/utils/blue_captcha/javascript/blue_captcha.js" type="text/javascript"></script>
+    <script src="https://www.google.com/recaptcha/api/js/recaptcha_ajax.js" type="text/javascript"></script>  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript">!window.jQuery && document.write(unescape('%3Cscript src="/ext/jquery/jquery-1.4.1.min.js"%3E%3C/script%3E'))</script>
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/jquery/cookie/jquery.cookie.min.js" type="text/javascript"></script>
+    <script src="/modules/spud/js/Spud.js.php" type="text/javascript"></script>
+    <script type="text/javascript">window.bQuery = jQuery.noConflict(true);</script>
+    <link rel="stylesheet" href="https://s.bsd.net/page/static/20170314.3/css/jquery_ui_reset.css" type="text/css">
+    <script src="https://s.bsd.net/page/static/20170314.3/javascript/bsd.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/utils/locale/javascript/locale.class.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/modules/signup/javascript/locale.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/modules/signup/javascript/onsubmit.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/utils/ajax/ajax.class.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/jquery/bbq/jquery.ba-bbq.min.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/modules/signup/javascript/form_defaults.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/modules/signup/javascript/form_spud.js" type="text/javascript"></script>
+    <script src="https://s.bsd.net/page/static/20170314.3/ext/jquery/mailcheck/jquery.mailcheck.js" type="text/javascript"></script>
+    <!-- END BSD SET UP -->
+    <!-- RiseUp Google Analytics -->
+   
+    <!-- End Google Analytics -->   
+</head>
+<body id="page-top">
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-left page-scroll" href="#page-top">
+                    <img class="logo-hide" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/riseup-sm-logo.png">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    <li>
+                   <!--  <li>
+                        <a class="page-scroll" href="#torchbearer">Torchbearer</a>
+                    <li> -->
+                        <a class="page-scroll" href="#postcard">Action</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#event">Event</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#gearup">Gear</a>
+                    </li>
+                     <li>
+                        <a class="page-scroll" href="#donate">Donate</a>
+                    </li>
+                 <!--    <li>
+                        <a href="https://reformimmigrationforamerica.org/rise-up-es/">En Español</a>
+                    </li> -->
+                    <li>
+                        <a class="page-scroll" href="https://www.facebook.com/reformimmigrationforamerica/"><img style="margin-top: -5px;" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/fb-16-blue.png" ></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="https://twitter.com/ri4a"><img style="margin-top: -5px;" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/twitter-16-blue.png" ></a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <header>
+        <div class="container rise-up-about">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-1">
+                    <div class="header-content">
+                        <div class="header-content-inner">
+                             <div id="svg-logo-container2">
+                                   <img class="logo-img img-responsive" src="https://reformimmigrationforamerica.org/wp-content/uploads/2017/03/riseup-logo-updated.png">
+                            </div><!-- end of svg-logo-container2" -->
+                            <h3 id="rise-up-openning-text">  Our country is strongest when we are united. Yet certain politicians seek to set us against one another for their personal gain and profit. If you’re ready to resist, become a torchbearer! <br>
+
+                            Torchbearers are a group of people ready to RISE UP and stop attacks against immigrants. Torchbearers believe that any attack on immigrants is an attack on all of us. On May 1st, we will rise up to defend our immigrant neighbors and friends!
+                             <!--    <br><br>
+                                <a style="color:#34CFBE;" href="#about" class="btn btn-outline btn-xl page-scroll">Learn More</a> -->
+                            </h3>
+                            <div class="rise-up-openning-text-container"></div>
+                            <br><br>
+                            <!-- <a href="#about" class="btn btn-outline btn-xl page-scroll">Learn More</a> -->
+                            <div>
+                                <?php echo do_shortcode('[contact-form-7 id="4218" title="Torchbearer Signup"]'); ?>
+                            </div>
+                            <hr>
+                        </div>
+                        <div class="overlay"></div>  
+
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div class="device-container">
+                        <!-- <div class="device-mockup iphone6_plus portrait white"> -->
+                        <div class="white">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+  
